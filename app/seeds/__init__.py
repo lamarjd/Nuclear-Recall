b71b54e7f787254c289d7cb74221a28d4491237c
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-11 19:11:13 -0500

    list can be populated with seed data

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .list import seed_lists, undo_lists
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,7 +18,9 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_lists()
     seed_users()
+    seed_lists()
     # Add other seed functions here
 
 
@@ -25,4 +28,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_lists()
     # Add other undo functions here
