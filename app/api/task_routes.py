5c1ab95f8f3db14603fe1a6468f90bab5d42fc87
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-12 12:26:12 -0500

    update

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -2,4 +2,19 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import Task
 
+#import models
+
+from ..models import Task, User
+
 task_routes = Blueprint('tasks', __name__)
+
+
+@task_routes.route('/')
+
+def get_all_tasks():
+    tasks = Task.query.all()
+  
+    
+
+
+
