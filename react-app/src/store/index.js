cafab7cbc4e1bbf4b5cfd781f571035d4ad8a6ac
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-13 12:33:38 -0500

    up and running

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import task from './tasks'
 
 const rootReducer = combineReducers({
   session,
+  task
 });
 
 
