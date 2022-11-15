9edb5e36e2f3b70f905c59c5cc0d972a0595efc6
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-15 16:38:20 -0500

    looks better than it was

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -3,31 +3,33 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 
+import './navbar.css'
+
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav id='navidout'>
+      <ul className='navnavbar'>
+        <li className='listnavbar'>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
           </NavLink>
         </li>
-        <li>
+        <li className='listnavbar'>
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
         </li>
-        <li>
+        <li className='listnavbar'>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
         </li>
-        <li>
+        <li className='listnavbar'>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
         </li>
-        <li>
+        <li className='listnavbar'>
           <LogoutButton />
         </li>
       </ul>
