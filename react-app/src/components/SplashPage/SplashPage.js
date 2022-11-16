d497282387e73959a71e4b83c554696080ea7955
lamarjd <jacob.lamar16@gmail.com>
2022-11-16 11:12:27 -0800

    Splash page boxed in, need login button to work

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -16,29 +16,25 @@ function SplashPage({ user }) {
     <>
       {!user && (
         <div className="splash-container">
-          <div className="splash-content">
+          {/* <div className="splash-content"> */}
             <div className="splash-picture-container">
               <div className="top-left">
                 <img id="logo" alt="logo" src={logo} />
               </div>
 
-              <div className="top-right">
-                
-              </div>
-
               <div className="center">
-                <h1>Say no to the Nuclear Fallout</h1>
-                <h3>It's time to take charge of your day and start being productive, which means to</h3> 
+                <h1>Say no to the Nuclear Thought Fallout</h1>
+                {/* <h3>It's time to take charge of your day and start being productive, which means to</h3>  */}
                 
-                <h3 className="underline">STOP FORGETTING THINGS</h3>
+                <h3 className="underline">Log in and STOP FORGETTING THINGS</h3>
 
-                <h5>For Real Just Stop</h5>
-        
+                {/* <h5>For Real Just Stop</h5> */}
+                <LoginFormModal/>
                 
               </div>
             </div>{" "}
             <br />
-          </div>
+          {/* </div> */}
         </div>
       )}
     </>
