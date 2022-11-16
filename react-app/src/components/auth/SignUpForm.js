675aaa7bc9b23470606f60a72c794c994ccfcdf4
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-16 16:22:02 -0500

    signup doesnt work but progress made

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -7,6 +7,8 @@ const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
   const user = useSelector(state => state.session.user);
@@ -15,7 +17,7 @@ const SignUpForm = () => {
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+      const data = await dispatch(signUp(username, email, password, firstName, lastName));
       if (data) {
         setErrors(data)
       }
@@ -37,6 +39,14 @@ const SignUpForm = () => {
   const updateRepeatPassword = (e) => {
     setRepeatPassword(e.target.value);
   };
+
+  const updateFirstName = (e) => {
+    setFirstName(e.target.value)
+  }
+
+  const updateLastName = (e) => {
+    setLastName(e.target.value)
+  }
 
   if (user) {
     return <Redirect to='/' />;
@@ -67,6 +77,27 @@ const SignUpForm = () => {
           value={email}
         ></input>
       </div>
+    
+      <div>
+          <label>First Name</label>
+          <input
+          type='text'
+          name='firstName'
+          onChange={updateFirstName}
+          value={firstName}
+        ></input>
+      </div>
+
+      <div>
+          <label>Last Name</label>
+          <input
+          type='text'
+          name='lastName'
+          onChange={updateLastName}
+          value={lastName}
+        ></input>
+      </div>
+
       <div>
         <label>Password</label>
         <input
@@ -76,6 +107,7 @@ const SignUpForm = () => {
           value={password}
         ></input>
       </div>
+
       <div>
         <label>Repeat Password</label>
         <input
