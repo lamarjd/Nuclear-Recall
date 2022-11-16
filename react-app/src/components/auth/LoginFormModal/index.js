33844c29f67ff24873941c57ae9b8ac2c07d4edf
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-16 12:17:49 -0500

    added modal

---
+++ react-app/src/components/auth/LoginFormModal/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { Modal } from '../../../context/Modal';
+import LoginForm from './LoginForm';
+import "./loginForm.css"
+
+function LoginFormModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button className="sign-in" onClick={() => setShowModal(true)}>Log In</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <LoginForm />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default LoginFormModal;
