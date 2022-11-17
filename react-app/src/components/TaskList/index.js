a9759d968998e0f8ae2fdcd4f61465904b919822
lamarjd <jacob.lamar16@gmail.com>
2022-11-17 15:06:52 -0800

    Task list moved into Home Page

--- react-app/src/components/TaskList/index.js
+++ react-app/src/components/TaskList/index.js
@@ -1,55 +1,56 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import * as sessionActions from "../../store/session.js";
 
-import React from 'react'
-import { useEffect, useState } from 'react';
-import * as sessionActions from '../../store/session.js';
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, useHistory, useParams } from "react-router-dom";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useHistory, useParams } from 'react-router-dom';
+import { deleteTaskThunk, fetchTasks } from "../../store/tasks";
 
+import TaskForm from "../TaskForm/index.js";
+import "./TaskList.css";
 
+export default function AllTasks() {
+  const dispatch = useDispatch();
+  const reduxstate = useSelector((state) => state.tasks);
+  const listsState = useSelector((state) => state.lists);
 
-import { deleteTaskThunk, fetchTasks } from '../../store/tasks';
+  const thisUser = useSelector((state) => state.session.user);
+  console.log("user", thisUser);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-import TaskForm from '../TaskForm/index.js';
-import "./TaskList.css"
+  useEffect(() => {
+    dispatch(fetchTasks()).then(() => setIsLoaded(true));
+  }, [dispatch, listsState]);
 
+  const taskList = Object.values(reduxstate);
+  console.log(taskList);
 
-export default function AllTasks(){
+  return (
+    isLoaded && (
 
-    const dispatch = useDispatch();
-    const reduxstate = useSelector((state) => state.tasks);
-    const listsState = useSelector((state)=> state.lists)
-
-    const thisUser = useSelector(state => state.session.user);
-    console.log("user",thisUser)
-    const [isLoaded, setIsLoaded] = useState(false)
-
-    useEffect(() => {
-        dispatch(fetchTasks())
-            .then(() => setIsLoaded(true))
-    }, [dispatch,listsState])
-
-
-    const taskList = Object.values(reduxstate)
-    console.log(taskList)
-
-    return isLoaded && (
-        
-        
-        <div className='all-tasks-container'>
-        <h1>Tasksss</h1>
-        <TaskForm/>
-        {taskList.map(task => (
+        <div className="all-tasks-container">
+          <h1>Tasksss</h1>
+          <TaskForm />
+          {taskList.map((task) => (
             <div>
-                <hr />
-            <NavLink className="detail-navlink" key={task.id} to={`/all/${task.id}`}> <h3>{task.body}</h3></NavLink>
-            <button onClick={()=>dispatch(deleteTaskThunk(task.id))}> DELETE</button>
-                <hr />
+              <hr />
+              <NavLink
+                className="detail-navlink"
+                key={task.id}
+                to={`/all/${task.id}`}
+              >
+                {" "}
+                <h3>{task.body}</h3>
+              </NavLink>
+              <button onClick={() => dispatch(deleteTaskThunk(task.id))}>
+                {" "}
+                DELETE
+              </button>
+              <hr />
             </div>
-        ))}
-    </div>
-
-
-        )
-
-    }
+          ))}
+        </div>
+    )
+  );
+}
