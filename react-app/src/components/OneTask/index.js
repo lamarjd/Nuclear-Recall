84a2de3272a5d97d3e153f7d4a9587ca3a39aaca
Richard-M-Baine <efdeliveryservice@gmail.com>
2022-11-17 13:28:31 -0500

    more styling

--- react-app/src/components/OneTask/index.js
+++ react-app/src/components/OneTask/index.js
@@ -41,7 +41,7 @@ export default function OneTask() {
     isLoaded && (
       <div className="mainTaskDetailsOutDiv">
         <div className="outerTaskDetailsLeftDiv">
-        <h1>Options</h1>
+        <h1>Task Options</h1>
         <EditTaskListForm filtered={filtered} />
         
         <EditForm filtered={filtered} />
