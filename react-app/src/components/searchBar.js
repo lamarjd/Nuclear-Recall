d32288140509c4a68905f8b2ae9a80fe41bc0d52
christopher <cpannella3@gmail.com>
2022-11-20 15:23:26 -0500

    dont care

--- react-app/src/components/searchBar.js
+++ react-app/src/components/searchBar.js
@@ -13,38 +13,37 @@ const SearchBar = () => {
     if(!searchInput) return null
 
    return tasks.filter(task => task.body.toLowerCase().includes(searchInput.toLowerCase()) && task.complete ==false)
-  
+
   }
 
     const [searchInput, setSearchInput] = useState("")
     const taskState = useSelector(state => state.tasks)
     const dispatch= useDispatch()
     const tasks = Object.values(taskState)
-    
+
 
     const tasksFound = matchingTask(searchInput,tasks)
 
   return (
 
-    <div SEARCH BAR>
+    <div className="searchbar">
       <input className="searchField"
-    id="search"
-    type="text"
-    placeholder="Search Tasks Here"
-    onChange={e => setSearchInput(e.target.value)}
-  //  value={searchInput}
-   />
-    <div>
-      {tasksFound &&
-        <div className="searchResults">
-          {tasksFound?.map((oneTask) =>(
-            <NavLink onClick={()=> (setSearchInput(""))} to={`/all/${oneTask.id}`}>
-            <div key={oneTask.id}>{oneTask.body}</div>
-            </NavLink>
-          ))}
+      id="search"
+      type="text"
+      placeholder="Search Tasks Here"
+      onChange={e => setSearchInput(e.target.value)}
+      />
+      <div>
+        {tasksFound &&
+          <div className="searchResults">
+            {tasksFound?.map((oneTask) =>(
+              <NavLink onClick={()=> (setSearchInput(""))} to={`/all/${oneTask.id}`}>
+              <div className="individual-result" key={oneTask.id}>{oneTask.body}</div>
+              </NavLink>
+            ))}
+          </div>
+          }
         </div>
-        }
-      </div>
     </div>
   );
 };
