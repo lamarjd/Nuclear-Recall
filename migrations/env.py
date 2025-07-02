beb555fc9c565714c9cd7a3013ac7b51a34bf589
lamarjd <jacob.lamar16@gmail.com>
2025-07-02 12:18:32 -0700

    Modified env.py with correct schema config

--- migrations/env.py
+++ migrations/env.py
@@ -1,56 +1,39 @@
 from __future__ import with_statement
 import os
-
 import logging
 from logging.config import fileConfig
 
 from flask import current_app
-
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# Alembic Config
 config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-config.set_main_option(
-    'sqlalchemy.url',
-    str(current_app.extensions['migrate'].db.get_engine().url).replace(
-        '%', '%%'))
-target_metadata = current_app.extensions['migrate'].db.metadata
-
+# Grab schema from environment
 schema = os.getenv("SCHEMA")
+db = current_app.extensions['migrate'].db
+target_metadata = db.metadata
+
+# Set metadata schema for generation
 target_metadata.schema = schema
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
+# Inject current DB URL (for offline mode)
+config.set_main_option(
+    'sqlalchemy.url',
+    str(db.get_engine().url).replace('%', '%%')
+)
 
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
+    """Run migrations without DB connection (offline mode)."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        include_schemas=True,
+        version_table_schema=schema  # 💡 stores alembic_version table in schema
     )
 
     with context.begin_transaction():
@@ -58,16 +41,7 @@ def run_migrations_offline():
 
 
 def run_migrations_online():
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-
-    # this callback is used to prevent an auto-migration from being generated
-    # when there are no changes to the schema
-    # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
+    """Run migrations with DB connection (online mode)."""
     def process_revision_directives(context, revision, directives):
         if getattr(config.cmd_opts, 'autogenerate', False):
             script = directives[0]
@@ -75,13 +49,19 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info('No changes in schema detected.')
 
-    connectable = current_app.extensions['migrate'].db.get_engine()
+    connectable = db.get_engine()
 
     with connectable.connect() as connection:
+        if schema:
+            connection.execute(f'CREATE SCHEMA IF NOT EXISTS {schema}')
+            connection.execute(f'SET search_path TO {schema}')
+
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
             process_revision_directives=process_revision_directives,
+            include_schemas=True,
+            version_table_schema=schema,  # 💡 prevents versioning conflict
             **current_app.extensions['migrate'].configure_args
         )
 
